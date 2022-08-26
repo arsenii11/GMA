6c65b985139bdfc99e9740d5f1ec24c32023d08d
arsenii11 <arseniy2004fgh@gmail.com>
2022-08-26 16:18:15 +0300

    button fix

---
+++ app/src/main/java/com/example/gma/Documents.kt
@@ -0,0 +1,11 @@
+package com.example.gma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Documents : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_documents)
+    }
+}
--- app/src/main/java/com/example/gma/ui/account/AccountsFragment.kt
+++ app/src/main/java/com/example/gma/ui/account/AccountsFragment.kt
@@ -28,10 +28,6 @@ class AccountsFragment : Fragment() {
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
