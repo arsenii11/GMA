5eb2ca337d1a4c9f0c14ccd95a8091406ff1e330
arsenii11 <arseniy2004fgh@gmail.com>
2022-09-26 20:35:52 +0300

    Edit button

--- app/src/main/java/com/example/gma/ui/account/AccountsFragment.kt
+++ app/src/main/java/com/example/gma/ui/account/AccountsFragment.kt
@@ -1,16 +1,20 @@
 package com.example.gma.ui.account
 
+import android.Manifest
 import android.R
 import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
@@ -24,7 +28,7 @@ private const val ACTIVITY = "ACCOUNT"
 private const val GALLERY_REQUEST = 1
 const val APP_PREFERENCES_Path = "Nickname"
 var profile: SharedPreferences? = null
-
+private const val PICK_IMAGE_REQUEST = 1
 var FilePath = ""
 
 class AccountsFragment : Fragment() {
@@ -50,9 +54,19 @@ class AccountsFragment : Fragment() {
         val root: View = binding.root
 
         //opening gallery to choose an image
-        binding.imageButton3.setOnClickListener{
-            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-            startActivityForResult(gallery, pickImage)
+        binding.imageButton3.setOnClickListener { v ->
+            if (activity?.checkSelfPermission( Manifest.permission.READ_EXTERNAL_STORAGE)  !== PackageManager.PERMISSION_GRANTED &&
+                activity?.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)  !== PackageManager.PERMISSION_GRANTED
+            ) {
+                val permissions = arrayOf<String>(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                activity?.let { ActivityCompat.requestPermissions(it, permissions, 1) }
+            }
+            val photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+            startActivityForResult(photoPickerIntent, GALLERY_REQUEST)
         }
 
 
@@ -61,14 +75,29 @@ class AccountsFragment : Fragment() {
         return root
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+  /*  @Override
+    public void onActivityResult(var requestCode, val resultCode, val data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK && requestCode == pickImage) {
-            imageUri = data?.data
-            binding.profilePic.setImageURI(imageUri)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            profileImage =  findViewById(R.id.ProfileImage);
+            profileImage.setImageResource(R.drawable.no_avatar);
+            selectedImageUri = data.getData();
+            Bitmap bitmap;
+            //Сохраняем изображение в файл
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                new fileFromBitmap("ProfileFoto", bitmap, getApplicationContext()).execute();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
         }
     }
-
+*/
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
