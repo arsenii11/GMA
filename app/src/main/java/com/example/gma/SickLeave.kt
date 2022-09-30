2e2b5703460e4e789f7d594afbc2802e55cb977b
arsenii11 <arseniy2004fgh@gmail.com>
2022-09-30 04:28:21 +0300

    Design fix

--- app/src/main/java/com/example/gma/SickLeave.kt
+++ app/src/main/java/com/example/gma/SickLeave.kt
@@ -1,11 +1,14 @@
 package com.example.gma
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.gma.databinding.ActivitySickLeaveBinding
 import com.example.gma.databinding.ActivityStudyBinding
+import java.util.Calendar
 
 
 class SickLeave : AppCompatActivity() {
@@ -16,10 +19,26 @@ class SickLeave : AppCompatActivity() {
         setContentView(R.layout.activity_sick_leave)
         binding = ActivitySickLeaveBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //Return button
       binding.backbuttongr.setOnClickListener{
             val randomIntent = Intent(this, Documents::class.java)
             startActivity(randomIntent)
           finish()
+        }
+
+        //Data picker
+        val c = Calendar.getInstance()
+        val year =c.get(Calendar.YEAR)
+        val month =c.get(Calendar.MONTH)
+        val data = c.get(Calendar.DAY_OF_MONTH)
+
+        //button click to show DataPicker
+        binding.editTextDate.setOnClickListener {
+            val dpd = DatePickerDialog(this,DatePickerDialog.OnDateSetListener{view, mYear, mMonth, mDay ->
+
+                binding.editTextDate.text = ("/"+mDay+"/"+mMonth+"/"+mYear)
+            },year, month,data)
+            dpd.show()
         }
     }
 }
