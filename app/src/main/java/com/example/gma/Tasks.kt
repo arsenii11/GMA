f9117f21888d3006816f2332ae5a6da45ef0df71
arsenii11 <arseniy2004fgh@gmail.com>
2022-11-13 00:12:39 +0200

    button added to tasks list

--- app/src/main/java/com/example/gma/Tasks.kt
+++ app/src/main/java/com/example/gma/Tasks.kt
@@ -1,24 +1,117 @@
 package com.example.gma
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
-import com.ramotion.foldingcell.FoldingCell
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.edit
+import androidx.core.view.GravityCompat
+import androidx.core.view.isVisible
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gma.R
+import com.example.gma.utils.bindView
+import com.google.android.material.appbar.AppBarLayout
+import com.nikhilpanju.fabfilter.main.MainListAdapter
+import com.nikhilpanju.fabfilter.main.ToolbarBehavior
+
+
+var animationPlaybackSpeed: Double = 0.8
 
 class Tasks : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tasks_list)
+
+
+        private val recyclerView: RecyclerView by bindView(R.id.recycler_view)
+        // layout/nav_drawer views
+
+        private lateinit var mainListAdapter: MainListAdapter
+        private val loadingDuration: Long
+            get() = (resources.getInteger(R.integer.loadingAnimDuration) / animationPlaybackSpeed).toLong()
+
+        /**
+         * Used to open nav drawer when opening app for first time (to show options)
+         */
+        private val prefs: SharedPreferences
+            get() = getSharedPreferences("FabFilter", Context.MODE_PRIVATE)
+        private var isFirstTime: Boolean
+            get() = prefs.getBoolean("isFirstTime", true)
+            set(value) = prefs.edit { putBoolean("isFirstTime", value) }
+        /**
+         * Used by FiltersLayout since we don't want to expose mainListAdapter (why?)
+         * (Option: Combine everything into one activity if & when necessary)
+         */
+        var isAdapterFiltered: Boolean
+            get() = mainListAdapter.isFiltered
+            set(value) {
+                mainListAdapter.isFiltered = value
+            }
+
+        ///////////////////////////////////////////////////////////////////////////
+        // Methods
+        ///////////////////////////////////////////////////////////////////////////
+
+        @SuppressLint("SetTextI18n")
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_tasks_list)
+
+
+            // RecyclerView Init
+            mainListAdapter = MainListAdapter(this)
+            recyclerView.adapter = mainListAdapter
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            recyclerView.setHasFixedSize(true)
+            updateRecyclerViewAnimDuration()
+
+
+            lateinit var backbutton:Button
+            backbutton = findViewById(R.id.backbutton)
+            backbutton.setOnClickListener{
+                val randomIntent = Intent(this, MainActivity::class.java)
+                startActivity(randomIntent)
+            }
 
 
 
-        // get our folding cell
-       val fc:FoldingCell=  findViewById(R.id.folding_cell)
-
-        // attach click listener to folding cell
-        fc.setOnClickListener{
-
-                fc.toggle(false)
+            // Open Nav Drawer when opening app for the first time
 
         }
+
+        /**
+         * Callback for motionLayoutCheckbox
+         * isChecked = true -> Use [FiltersMotionLayout]
+         * isChecked = false -> Use [FiltersLayout]
+         */
+
+
+        /**
+         * Update RecyclerView Item Animation Durations
+         */
+        private fun updateRecyclerViewAnimDuration() = recyclerView.itemAnimator?.run {
+            removeDuration = loadingDuration * 60 / 100
+            addDuration = loadingDuration
+        }
+
+        /**
+         * Open browser for given string resId URL
+         */
+        private fun openBrowser(@StringRes resId: Int): Unit =
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(resId))))
+
+        /**
+         * Called from FiltersLayout to get adapter scale down animator
+         */
+        fun getAdapterScaleDownAnimator(isScaledDown: Boolean): ValueAnimator =
+            mainListAdapter.getScaleDownAnimator(isScaledDown)
     }
-}
---
+++ app/src/main/java/com/example/gma/views/NoScrollRecyclerView.kt
@@ -0,0 +1,28 @@
+package com.example.gma.views
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * A hack-job way to allow a RecyclerView to be scrolled only programmatically and not via touch inputs
+ */
+class NoScrollRecyclerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
+    : RecyclerView(context, attrs, defStyleAttr) {
+
+    override fun scrollBy(x: Int, y: Int) {
+        val lm = layoutManager as? NoScrollHorizontalLayoutManager
+        lm?.canScrollHorizontally = true
+        super.scrollBy(x, y)
+        lm?.canScrollHorizontally = false
+    }
+}
+
+class NoScrollHorizontalLayoutManager(context: Context)
+    : LinearLayoutManager(context, RecyclerView.HORIZONTAL, false) {
+
+    var canScrollHorizontally = false
+
+    override fun canScrollHorizontally(): Boolean = canScrollHorizontally
+}
