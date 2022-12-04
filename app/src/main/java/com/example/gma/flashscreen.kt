33e0d9bf22ccce90de50f64ffd5e089b13474c67
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2022-12-05 00:56:46 +0200

    Splascreen fix

--- app/src/main/java/com/example/gma/flashscreen.kt
+++ app/src/main/java/com/example/gma/flashscreen.kt
@@ -15,29 +15,7 @@ class flashscreen : AppCompatActivity() {
         val iv_note: ImageView by lazy { findViewById(R.id.rediss) }
         iv_note.alpha = 0f
         iv_note.animate().setDuration(2000).alpha(1f).withEndAction() {
-
-            //обращаемся к настройкам
-            val sp = getSharedPreferences(
-                "hasVisited", Context.MODE_PRIVATE
-            )
-            // проверяем, первый ли раз открывается программа (Если вход первый то вернет false)
-            val hasVisited = sp.getBoolean("hasVisited", false)
-
             val i = Intent(this, MainActivity::class.java)
-
-            if (!hasVisited) {
-                // Сработает если Вход первый
-                //Ставим метку что вход уже был
-                val e = sp.edit()
-                e.putBoolean("hasVisited", true)
-                e.apply()
-
-                i.putExtra("VISITED", false);
-            }
-            else {
-                i.putExtra("VISITED", true);
-            }
-
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
